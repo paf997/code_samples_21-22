84958388f0a45b786379e366a08612f94ceac6ec
paf997 <55116984+paf997@users.noreply.github.com>
2022-06-16 15:45:31 -0600

    Custom WP theme. HTML, SCC, Javascript and PHP
    https://projectdym.000webhostapp.com/

---
+++ footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <footer>
+        <div class="footer">
+            <div class="footer_div">
+                <div>
+                    <div class="footer_header">Contact Us</div>
+                    <div>Dymaco Inc.</div>
+                    <div>30 St.Pauls St.</div>
+                    <div>Thunder Bay, ON, P7A 4S5</div>
+                    <div> info@dymaco.ca</div>
+                </div>
+                <div>
+                    <div class="footer_header" onclick="reveal_quick_links()">Quick Links</div>
+                    <div id="quick_links">
+                        <div><a href="#"> About</a></div>
+                        <div><a href="https://projectdym.000webhostapp.com/page_2"> Services</a></div>
+                        <div><a href="https://projectdym.000webhostapp.com/page_3"> Team</a></div>
+                        <div><a href="https://projectdym.000webhostapp.com/page_4"> Gallery</a></div>
+                    </div>
+                </div>
+            </div>
+                &copy; 2021 Dymaco Inc.
+        </div>
+    </footer>
+
+<?php wp_footer();?>
---
+++ front-page.php
@@ -0,0 +1,68 @@
+<?php get_header();?>
+
+
+
+    <div class="text_and_image_div_white_on_black extra_top_bottom_padding text_and_banner_padding">
+        Dymaco is a professional engineering and consulting firm based in Thunder Bay, Ontario.     
+        Our specialized team provides a range of services to a diverse roster of clients involved in 
+        resource-based industrial and commercial endeavors across Canada and around the world.
+ 
+    </div>
+
+    <div class="text_and_image_div_white_on_black second_background_img text_and_banner_padding">
+		
+    We are innovative problem solvers with rich experience in the preparation of tender documents, project management, construction supervision, quality control, specification preparation, site evaluation/auditing, and more.
+
+    Dymaco maintains an extensive network of specialists that guarantee the provision of a broad scope of multi-disciplinary services, all while ensuring Dymaco’s clients enjoy the benefits of a primary project consultant.
+    </div>
+
+    <div class="card_display">
+        <a href="https://dymaco.flywheelsites.com/?page_id=117" class="picture_card">
+            Services
+		</a>
+
+        <a href="https://dymaco.flywheelsites.com/?page_id=113" class="picture_card card2_img">
+            Team
+		</a>
+        <a href="https://dymaco.flywheelsites.com/projects/#projects_gallery" class="picture_card card3_img">
+            Project Gallery
+        </a>
+    </div>
+
+    <div class="text_and_image_div_white_on_black black_bg_img less_padding">
+        <div class="underlined_header">
+            VISION
+        </div>
+        We believe our products and services must be done efficiently, safely, and held to the highest technical standard. Our professional transparency highlights our expertise and understanding of engineering. Our specialized team is the new competitive standard for heavy industrial engineering.
+    </div>
+
+
+
+    <div class="text_and_image_div_white_on_black third_background_img text_and_banner_padding">
+        <div class="underlined_header">
+            MISSION
+        </div>  
+        Dymaco’s experienced team strives to offer the finest professional services, while expanding throughout North America and internationally.   
+    </div>
+
+
+<?php get_footer();?>
+
+
+
+
+
+
+
+
+
+    
+    
+
+    
+
+
+
+
+
+
