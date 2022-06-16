dd5522af0ab56e710491ffce7408609f7ce80ada
paf997 <55116984+paf997@users.noreply.github.com>
2022-06-16 15:19:19 -0600

    Add files via upload
    Code excerpts for a project I did for a client using. Created a WP theme using CSS, HTML, Javascript and PHP

---
+++ dymaco_theme/Confirmation_Page.php
@@ -0,0 +1,3 @@
+<?php
+	/* Template Name: Confirmation_Page */
+?>
---
+++ dymaco_theme/page4.php
@@ -0,0 +1,90 @@
+<?php
+	/* Template Name: page4 */
+?>
+		
+<?php get_header();?>
+
+<div class="text_and_image_div_white_on_black just_header_banner text_and_banner_padding">
+    <div class="experience header"> SERVICES</div>
+</div>
+
+<div class="text_and_image_div_white_on_black black_bg_img less_padding">
+    <div class="header">STRUCTURAL ENGINEERING</div>
+        With extensive experience in the heavy industrial sector, including mining, fixed plant,
+    materials handling, process infrastructure, power generation, and the pulp and paper
+    industry, Dymaco provides structural engineering services to a wide variety of project in
+    various stages of development.
+</div>
+
+    <div class="column_div white_text black_bg">
+        Structural Engineering Services:
+        <div class="two_column_grid white_text">
+            <div>
+                <ul class="white_text">
+                    <li>Design of complex steel, concrete, masonry, and timber structures</li>
+                    <li>Development of greenfield brownfield operations improvement design </li>
+                    <li>Design of shallow and deep foundations, including pilings and spread footings</li>
+                    <li>Fatigue and dynamic analysis, frequencies and cyclic loading through to vibratory
+effects</li>
+                </ul>
+            </div>
+            <div>
+                <ul class="white_text">
+                    <li>Specialized heavy lift design, including rigging and lifting procedures</li>
+                    <li>Site assistance, audits, and certifications</li>
+                    <li>Structural remediation</li>
+                    <li>Construction engineering</li>
+                    <li>Scaffold design</li>
+                    <li>Third-party engineering reviews</li>
+                </ul> 
+            </div>
+        </div>
+    </div>
+
+    <div class="text_and_image_div_black_on_white less_padding white_bgcolor_black_text common_text_size">
+        <div class="header">MINING SERVICES</div>
+            Dymaco specializes in providing services to the mining industry, with capabilities and
+        experience in technical personnel, open pit and underground mine design (shaft or ramp
+        access) sinking and development through to construction, production, operation and
+        maintenance, and regulations.
+</div>
+<div class="white_bg">
+    <div class="column_div white_bgcolor_">
+        Mining Services:
+        <div class="two_column_grid">
+            <div>
+                <ul>
+                    <li>Preliminary economic assessments, pre-feasibility, and feasibility studies (incl.
+                        preliminary detailed design for quote pricing)</li>
+                    <li>Surface sinking arrangements and detailed design for work stages/Galloway’s,
+                        portable sinking headframes and permanent sinking headframes capable for
+                        transition to permanent arrangements, sinking conveyances and buckets, and
+                        adjacent structures including winch foundations with rope diagrams</li>
+                    <li>Shaft design, including hoisting system design, selection and procurement, and
+                        headframe/surface plant design</li>
+                    <li>Mine support facility design including mine administration facilities, mine dry’s,
+                        shops, equipment housing structures, and tank farms</li>
+                </ul>
+            </div>
+            <div class="white_text">
+                <ul>
+                    <li>Material-handling muck and ore systems including underground loading pockets,
+                        bins, dumps, crushing, screening, conveying and stockpiling.</li>
+                    <li>Civil/structural reviews and upgrades for all mill facilities/structures, and
+                        foundations</li>
+                    <li>Fixed plant and logistics support, on site detailed engineering for immediate site
+                        requirements for operations and engineered logistic solutions (handling skids
+                        designed and certified for proper surface to underground solutions)</li>
+                    <li>Specialized mining design knowledge for steel and concrete headframe designs,
+                        sheave and hoist decks, arrestor steel, dump levels, collar and sub-collar
+                        solutions, shaft steel, rope requirements and arrangements, brattice, station
+                        levels, crash steel and shaft bottom
+					</br></br>
+						AND MORE...
+						</li>
+                </ul> 
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer();?>
