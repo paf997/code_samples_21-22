662d515c332a85029579b993875aeae136f1682c
paf997 <55116984+paf997@users.noreply.github.com>
2022-06-16 15:32:53 -0600

    Add files via upload
    Some of the custom components made for Ritornello

---
+++ BlockWysiwygTextWrap/functions.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Flynt\Components\BlockWysiwygTextWrap;
+
+use Flynt\FieldVariables;
+
+function getACFLayout()
+{
+    return [
+        'name' => 'blockWysiwygTextWrap',
+        'label' => 'Block: Wysiwyg Text Wrap',
+        'sub_fields' => [
+            [
+                'label' => __('General', 'flynt'),
+                'name' => 'generalTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0,
+            ],
+            [
+                'label' => __('Text Alignment', 'flynt'),
+                'name' => 'textAlignment',
+                'type' => 'button_group',
+                'choices' => [
+                    'textLeft' => '<i class=\'dashicons dashicons-editor-alignleft\' title=\'Align text left\'></i>',
+                    'textCenter' => '<i class=\'dashicons dashicons-editor-aligncenter\' title=\'Align text center\'></i>'
+                ]
+            ],
+            [
+                'label' => __('Content', 'flynt'),
+                'name' => 'contentHtml',
+                'type' => 'wysiwyg',
+                'delay' => 1,
+                'media_upload' => 0,
+                'required' => 1,
+            ],
+            [
+                'label' => __('Inner Content', 'flynt'),
+                'name' => 'inner_content',
+                'type' => 'wysiwyg',
+                'delay' => 1,
+                'media_upload' => 0,
+                'required' => 0,
+            ],
+            [
+                'label' => __('Options', 'flynt'),
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => '1st Column Theme',
+                'name' => 'options',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\getTheme()
+                ]
+            ],
+            [
+                'label' => '2nd Column Theme',
+                'name' => 'options_2',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\getTheme()
+                ]
+            ]
+
+        ]
+    ];
+}
